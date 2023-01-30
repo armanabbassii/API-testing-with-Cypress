831e7d120c427a8a3d9f9f15feca09205174a815
armanabbassii <arman.abbasi.nt@gmail.com>
2023-01-30 22:06:57 +0330

    push test

--- cypress/e2e/1-getting-started/APITesting.cy.js
+++ cypress/e2e/1-getting-started/APITesting.cy.js
@@ -1,5 +1,8 @@
 /// <reference types = "cypress"/>
-it ('google search', function() {
-    cy.request('https://restcountries.com/v3.1/name/germany')
 
-})
+const { Test } = require("mocha");
+
+it("google search", function () {
+  cy.request("https://restcountries.com/v3.1/name/germany");
+});
+Test;
