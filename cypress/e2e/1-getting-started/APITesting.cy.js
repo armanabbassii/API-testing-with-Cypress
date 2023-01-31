e637c5557798427ea92909dca44ac39ab8748f90
armanabbassii <arman.abbasi.nt@gmail.com>
2023-01-31 10:20:44 +0330

    test

--- cypress/e2e/1-getting-started/APITesting.cy.js
+++ cypress/e2e/1-getting-started/APITesting.cy.js
@@ -1,12 +1,18 @@
 /// <reference types = "cypress"/>
 
-it("google search", ()=> {
-  cy.request('https://restcountries.com/v3.1/name/germany').then(($response) => {
-    expect($response.status).to.eq(200)
-    expect($response.body[0].capital[0]).to.eq('Berlin')
-  })
+it("google search", () => {
+  cy.request("https://restcountries.com/v3.1/name/germany").then(
+    ($response) => {
+      expect($response.status).to.eq(200);
+      expect($response.body[0].capital[0]).to.eq("Berlin");
+    }
+  );
 });
 
 //for recieve response, use it "then"
 //   ش میکنیمyeild که ابجکتی که به ما برمیگرده رو
-// حالا ما ابجکت ریسپانس رو داریم. 
+// حالا ما ابجکت ریسپانس رو داریم.
+teststs;
+toString;
+erter;
+dgdg;
