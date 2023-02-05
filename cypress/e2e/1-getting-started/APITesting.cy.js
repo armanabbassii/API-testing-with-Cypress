1470d241ef10efe20f43a610454326bf660559f9
armanabbassii <arman.abbasi.nt@gmail.com>
2023-02-05 16:06:38 +0330

    fixbug

--- cypress/e2e/1-getting-started/APITesting.cy.js
+++ cypress/e2e/1-getting-started/APITesting.cy.js
@@ -15,10 +15,21 @@ it.only("API chaining", () => {
       expect($response.status).to.eq(200);
       let countryCurrencyKey = Object.keys($response.body[0]["currencies"]);
 
-      cy.request("https://localhost:3000/countries", {
-        name: "australia",
+      cy.request("POST", "https://localhost:3000/countries", {
+        name: "Australia",
         capital: $response.body[0].capital[0],
         currency: countryCurrencyKey[0],
+      });
+      cy.request("GET", "https://localhost:3000/countries").then(($resp) => {
+        let result = {};
+        $resp.body.forEach((item) => {
+          if ((item.name = "Australia")) {
+            result = item;
+          }
+        });
+        expect(result).to.not.empty;
+        expect(result.capital).to.eql("Canberra");
+        expect(result.currency).to.eql("AUD");
       });
     }
   );
