c096c4c84a48185a24c4e37b184dff22603c6618
Arman Abbasi <arman.abbasi.nt@gmail.com>
2023-02-12 19:16:25 +0330

    tst

--- cypress/e2e/1-getting-started/CMS-API-Testing.cy.js
+++ cypress/e2e/1-getting-started/CMS-API-Testing.cy.js
@@ -1,7 +1,7 @@
 /// <reference types = "cypress"/>
 
 
-it("content-type create", () => {
+it.only("content-type create", () => {
     cy.request(POST, "http://rad-qc.sandpod.ir/api/documentation#/Content-Type/store-api-core-content-types", {
         name: "api testing",
         code: "test123",
@@ -9,7 +9,7 @@ it("content-type create", () => {
         ig: "601:3AIG-0000601:3A0",
         allFields: true,
         description: "aaaaaa",
-        AccessToken: "4a8a10a71168416395110a67cb62405e.XzIwMjMy",
+        AccessToken: "9d4261f3d182433f9f7f86b541338232.XzIwMjMy",
         ClientId: "17959574q2f0347718971594ccd86f3f4"
         })
     })
