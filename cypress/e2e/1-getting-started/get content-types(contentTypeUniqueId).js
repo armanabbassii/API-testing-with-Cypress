d2f8ae27467f5a1de4fa9970eab286535ecaec6c
armanabbassii <arman.abbasi.nt@gmail.com>
2023-02-15 16:42:00 +0330

    test

---
+++ cypress/e2e/1-getting-started/get content-types(contentTypeUniqueId).js
@@ -0,0 +1,20 @@
+/// <reference types = "cypress"/>
+
+const { get } = require("cypress/types/lodash");
+
+describe("get content-type(contentTypeUniqueId)", () => {
+
+    let AccessToken = "71054454f7e148a0b263b764fc38d659.XzIwMjMy";
+
+    it('get content-type(unique_id)', () => {
+        
+        cy.request({
+            method: get,
+            url: 'http://rad-qc.sandpod.ir/api/core/content-types/',
+            headers: {
+                
+            }
+    })
+    })
+    
+}
