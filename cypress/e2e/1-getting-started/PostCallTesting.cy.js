971e8d56e0ae23cd5bcea538a50d2a70a8443b53
armanabbassii <arman.abbasi.nt@gmail.com>
2023-02-15 15:49:08 +0330

    test

--- cypress/e2e/1-getting-started/PostCallTesting.cy.js
+++ cypress/e2e/1-getting-started/PostCallTesting.cy.js
@@ -25,13 +25,19 @@ describe("post content-type", () => {
       },
     }).then((response) => {
       expect(response.status).to.eql(200);
-      expect(response.body.result).to.have.property("name");
+      expect(response.body.result[0]).to.have.deep.property(
+        "name[0]",
+        "contentTest"
+      );
       expect(response.body.result).has.property("code", "332244556");
       expect(response.result.metadata).has.property("ig", "601:IG-0000601:0");
       expect(response.result.metadata).has.property(
         "description",
         "testDiscription"
       );
+    });
+    cy.request({
+      method: "GET",
     });
   });
 });
