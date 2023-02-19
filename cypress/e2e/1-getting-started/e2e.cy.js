f50ec7d1511b3224835849060c082f208012c25b
armanabbassii <arman.abbasi.nt@gmail.com>
2023-02-19 17:15:03 +0330

    test

--- cypress/e2e/1-getting-started/e2e.cy.js
+++ cypress/e2e/1-getting-started/e2e.cy.js
@@ -1,14 +1,20 @@
 /// <reference types = "cypress"/>
 
-//it("google search", () => {
-//  cy.visit("https://google.com");
-//    cy.get("input[name =q]").type("cypress{enter}");
+describe("suit1", () => {
+  it("google search", () => {
+    cy.visit("https://google.com");
+    cy.get("input[name =q]").type("cypress{enter}");
+  });
 
-it("cypress website testing", () => {
-  cy.visit("https://www.cypress.io/");
-  cy.title().should(
-    "eq",
-    "JavaScript Web Testing and Component Testing Framework | cypress.io"
-  );
-  cy.title().should("include", "tests for your modern web applications");
+  it("cypress website testing", () => {
+    cy.visit("https://www.cypress.io/");
+    cy.title().should(
+      "eq",
+      "JavaScript Web Testing and Component Testing Framework | cypress.io"
+    );
+    cy.title().should(
+      "include",
+      "JavaScript Web Testing and Component Testing Framework"
+    );
+  });
 });
