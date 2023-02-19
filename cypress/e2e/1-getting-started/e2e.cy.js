5c9dd712d82e5160a6f400b8a7f672c24c19a6d2
armanabbassii <arman.abbasi.nt@gmail.com>
2023-02-19 16:23:07 +0330

    test

--- cypress/e2e/1-getting-started/e2e.cy.js
+++ cypress/e2e/1-getting-started/e2e.cy.js
@@ -1,6 +1,14 @@
 /// <reference types = "cypress"/>
 
-it("google search", () => {
-  cy.visit("https://google.com");
-  cy.get("input[name =q]").type("cypress{enter}");
+//it("google search", () => {
+//  cy.visit("https://google.com");
+//    cy.get("input[name =q]").type("cypress{enter}");
+
+it("cypress website testing", () => {
+  cy.visit("https://www.cypress.io/");
+  cy.title().should(
+    "eq",
+    "JavaScript Web Testing and Component Testing Framework | cypress.io"
+  );
+  cy.title().should("include", "tests for your modern web applications");
 });
