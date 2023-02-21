c57cdbc9c04dbdf0dc46ea1f119307cdfd835a00
Arman Abbasi <arman.abbasi.nt@gmail.com>
2023-02-21 23:59:23 +0330

    test

--- cypress/e2e/1-getting-started/findElement.cy.js
+++ cypress/e2e/1-getting-started/findElement.cy.js
@@ -2,6 +2,8 @@
 
 describe("name of second test", () => {
   it("name of testcase 1", () => {
-    cy.visit("https://www.play2.automationcamp.ir/index.html");
+    cy.visit("https://www.play2.automationcamp.ir/index.html")
+    cy.get('#fname').type('Arman');
   });
 });
+
