27d1ad239665fbcb0e1318df48938293c13d3c9a
armanabbassii <arman.abbasi.nt@gmail.com>
2023-02-22 14:56:05 +0330

    test

--- cypress/e2e/1-getting-started/findElement.cy.js
+++ cypress/e2e/1-getting-started/findElement.cy.js
@@ -1,19 +1,27 @@
 /// <reference types = 'cypress'/>
 
-describe("name of second test", () => {
+describe("2.1 name of second test", () => {
   it("name of testcase 1", () => {
-    cy.visit("https://www.play2.automationcamp.ir/index.html")
-    cy.get('#fname').type('Arman');
-  })
-  it('scroll',()=>{
+    cy.visit("https://www.play2.automationcamp.ir/index.html");
+    cy.get("#fname").type("Arman");
+  });
+  /*
+  it('2.2 scroll',()=>{
     cy.visit("https://datatables.net/examples/basic_init/scroll_x.html")
     cy.get(':nth-child(9) > :nth-child(9)').click()
   })
-  it('contain text ',()=>{
+
+  it('2.3 contains text ',()=>{
     cy.visit("https://www.play2.automationcamp.ir/index.html")
     cy.contains('This is your Sample Tooltip Option').click()
     cy.contains('label[for=moption]',' Option 2').click()
     cy.get('label[for=moption]').contains(' Option 2').click()
-
   })
-})
+*/
+  it("2.4 find relative element", () => {
+    cy.visit("https://www.play2.automationcamp.ir/index.html");
+    cy.get("#owc").children;
+    cy.get("#owc").children("[value = 'option 2']").check();
+    cy.contains("Singer").parent();
+  });
+});
