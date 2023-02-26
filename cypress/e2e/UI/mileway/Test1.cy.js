69f6231c3fee82e64bca23ae930aed8069407c86
Arman Abbasi <arman.abbasi.nt@gmail.com>
2023-02-27 00:12:38 +0330

    test

--- cypress/e2e/UI/mileway/Test1.cy.js
+++
@@ -1,9 +0,0 @@
-/// <reference types = "cypress"/>
-
-describe ("Test suit1", ()=>{
-    it("Testcase1",() => {
-        cy.visit("https://mileway.com/")
-        cy.get('span').contains('Where we are').click()
-        cy.get('div').contains('United Kingdom').click({ force: true })
-    })
-})
