5facac383ed1df0a74cce17f0064a750c1b37767
Arman Abbasi <arman.abbasi.nt@gmail.com>
2023-02-26 23:06:39 +0330

    testcase1

--- cypress/e2e/UI/mileway/Test1.cy.js
+++ cypress/e2e/UI/mileway/Test1.cy.js
@@ -3,8 +3,7 @@
 describe ("Test suit1", ()=>{
     it("Testcase1",() => {
         cy.visit("https://mileway.com/")
-        cy.get("span","Where we are")
+        cy.get('span').contains('Where we are').click()
+        cy.get('div').contains('United Kingdom').click({ force: true })
     })
-
-
 })
