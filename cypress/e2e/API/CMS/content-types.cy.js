d35f56d963ab624cf38ac48d12e360a43f81b11e
armanabbassii <arman.abbasi.nt@gmail.com>
2023-02-27 14:22:52 +0330

    test

--- cypress/e2e/API/CMS/content-types.cy.js
+++ cypress/e2e/API/CMS/content-types.cy.js
@@ -1,6 +1,6 @@
 /// <reference types = "cypress"/>
 
-describe("post content-type", () => {
+describe("content-type", () => {
   let AccessToken = "71054454f7e148a0b263b764fc38d659.XzIwMjMy";
 
   it("create content-type", () => {
@@ -8,9 +8,9 @@ describe("post content-type", () => {
       method: "POST",
       url: "http://rad-qc.sandpod.ir/api/core/content-types",
       hearders: {
-        "accept": "application/json",
-        "Access-Token":"84a1f13c148943ef9ce1067e9c95c650.XzIwMjMy",
-        "Client-Id":"17959574q2f0347718971594ccd86f3f4",
+        accept: "application/json",
+        "Access-Token": "84a1f13c148943ef9ce1067e9c95c650.XzIwMjMy",
+        "Client-Id": "17959574q2f0347718971594ccd86f3f4",
         "X-CSRF-TOKEN": "",
       },
       body: {
@@ -25,7 +25,7 @@ describe("post content-type", () => {
       },
     }).then((response) => {
       expect(response.status).to.eql(200);
-      console.log(response);
+      /*console.log(response);
       expect(response.body.errorCode).to.eql(406);
 
       expect(response.body.result.name).to.equal('contentTest')
@@ -34,10 +34,10 @@ describe("post content-type", () => {
       expect(response.body.result[0]).to.have.deep.property("name[0]","contentTest");
       expect(response.body.result).has.property("code", "332244556");
       expect(response.result.metadata).has.property("ig", "601:IG-0000601:0");
-      expect(response.result.metadata).has.property(
+      /*expect(response.result.metadata).has.property(
         "description",
         "testDiscription"
-      );
+      );*/
     });
   });
 });
