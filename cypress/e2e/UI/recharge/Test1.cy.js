7aadaacd3c4ca5179e6735d8bdc5709b065bfd59
armanabbassii <arman.abbasi.nt@gmail.com>
2023-02-28 13:48:56 +0330

    test

--- cypress/e2e/UI/recharge/Test1.cy.js
+++ cypress/e2e/UI/recharge/Test1.cy.js
@@ -1,17 +1,20 @@
 /// <reference types = "cypress"/>
 
-describe ("Test suit1", ()=>{
-    it("Testcase1",() => {
-        cy.visit("https://company.recharge.com/")
-        cy.get('.pr-md-5 > .h1').should('contain','The leading one-stop-shop for all ')
-        //cy.title().should('Recharge.com | Recharge.com | The leading one-stop-shop for all your branded payment needs')
-        cy.contains('The leading one-stop-shop for all ')
-        cy.contains('Get to know us').click()
-        
-        //cy.get('.link-blue>.span').should('Get to know us').click()
-        //cy.get('class="h1"').should('The leading one-stop-shop for all ')
-        //cy.get($h1.first()).to.contain('The leading one-stop-shop for all')
-        //cy.get('span').contains('Where we are').click()
-        //cy.get('div').contains('United Kingdom').click({ force: true })
-    })
-})
+describe("Test suit1", () => {
+  it("Testcase1", () => {
+    cy.visit("https://company.recharge.com/");
+    cy.get(".pr-md-5 > .h1").should(
+      "contain",
+      "The leading one-stop-shop for all "
+    );
+    //cy.title().should('Recharge.com | Recharge.com | The leading one-stop-shop for all your branded payment needs')
+    cy.contains("The leading one-stop-shop for all ");
+    cy.contains("Get to know us").click();
+    cy.get(".holder > .h1").should("contain", "Branded payments made ");
+    //cy.get('.link-blue>.span').should('Get to know us').click()
+    //cy.get('class="h1"').should('The leading one-stop-shop for all ')
+    //cy.get($h1.first()).to.contain('The leading one-stop-shop for all')
+    //cy.get('span').contains('Where we are').click()
+    //cy.get('div').contains('United Kingdom').click({ force: true })
+  });
+});
