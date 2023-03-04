fdfde76c177196b4796d5e92abb400cb85d5b881
armanabbassii <arman.abbasi.nt@gmail.com>
2023-03-04 14:25:22 +0330

    testcase1

--- cypress/e2e/UI/dataart/TestCase1.cy.js
+++
@@ -1,7 +0,0 @@
-/// <reference types = "cypress"/>
-
-describe("Testsuite1", () => {
-  it("TestCase1", () => {
-    cy.visit("https://www.dataart.team/");
-  });
-});
---
+++ cypress/e2e/UI/kidzy/TestCase1.cy.js
@@ -0,0 +1,22 @@
+/// <reference types = "cypress"/>
+
+describe("Testsuite1", () => {
+  it("TestCase1", () => {
+    cy.visit("https://kidzy.land");
+    cy.get(".intro-first").should("contain", "بچه‌ها ");
+  });
+  it.only("TestCase2", () => {
+    cy.visit("https://kidzy.land/aboutus");
+    cy.get("input[placeholder = 'نام و نام خانوادگی']").type("armanabbasi");
+    cy.get("#phone").type("09194292570");
+    cy.get("#email")
+      .type("arman.abbasi.nr@gmail.com")
+      .should("class", "rounded-4xl");
+    cy.get(".resize-none").type("Hi. this is Kidzy.land");
+    cy.get('input[placeholder="متن داخل تصویر"]').type("1122233");
+    cy.get('input[value="ارسال پیام"]').click();
+
+    //cy.get('[id="phone"][class=".rounded-4xl"]');
+    //cy.get((".rounded-4xl").click();)
+  });
+});
