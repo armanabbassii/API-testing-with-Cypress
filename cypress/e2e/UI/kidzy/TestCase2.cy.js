167d555a5fcba1678e6a44f95531090e092e6def
Arman Abbasi <arman.abbasi.nt@gmail.com>
2023-03-07 00:47:45 +0330

    tstcse

--- cypress/e2e/UI/kidzy/TestCase2.cy.js
+++ cypress/e2e/UI/kidzy/TestCase2.cy.js
@@ -4,6 +4,10 @@ describe("Carrers",()=>{
     it('testCase',()=>{
         cy.visit("https://kidzy.land/")
         cy.get('.middle').click()
+        cy.get(':nth-child(9) > .text-white').click()
+        cy.get(':nth-child(1) > .w-18\/20 > .w-fit').click()
+        cy.get('.flex-wrap > :nth-child(2) > h2').click()
+        cy.get('.jobs_upload__GFueB').click()
         //cy.get("path[class='middle']").click()
         //cy.get("a[href ='فرصت‌های شغلی']").click();
     })
