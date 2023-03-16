10887fca6dff7be545fa1811baeacd5c37008170
armanabbassii <arman.abbasi.nt@gmail.com>
2023-03-16 17:08:46 +0330

    testcase3

---
+++ cypress/e2e/UI/kidzy/TestCase3.cy.js
@@ -0,0 +1,10 @@
+/// <reference types = "cypress"/>
+
+describe("TestSuite3", () => {
+  it("testCase1", () => {
+    cy.visit("https://kidzy.land"),
+      cy.contains("همکاری با کیدزی"),
+      cy.contains("a[class = 'rounded-full']", "همکاری با کیدزی"),
+      cy.contains("a[class = 'rounded-full']").contains("همکاری با کیدزی");
+  });
+});
