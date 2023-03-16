db6401ef071bfe7b62464e7dffffdeaf7f8b9087
armanabbassii <arman.abbasi.nt@gmail.com>
2023-03-16 18:44:49 +0330

    testcase3

--- cypress/e2e/UI/kidzy/TestCase3.cy.js
+++ cypress/e2e/UI/kidzy/TestCase3.cy.js
@@ -4,7 +4,11 @@ describe("TestSuite3", () => {
   it("testCase1", () => {
     cy.visit("https://kidzy.land"),
       cy.contains("همکاری با کیدزی"),
-      cy.contains("a[class = 'rounded-full']", "همکاری با کیدزی"),
-      cy.contains("a[class = 'rounded-full']").contains("همکاری با کیدزی");
+      //cy.contains("a[class = 'rounded-full']", "همکاری با کیدزی"),
+      cy.contains("a[class='rounded-full']", "همکاری با کیدزی").click();
+    /* 
+      cy.get(".bg-gradient-to-b > .rounded-full").contains("همکاری با کیدزی").click();
+      cy.get(".rounded-full").contains("همکاری با کیدزی").click();
+      */
   });
 });
