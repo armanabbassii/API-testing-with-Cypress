d60851e7ba6468649d3f8230dc104e718e05f1b4
armanabbassii <arman.abbasi.nt@gmail.com>
2023-03-18 12:21:00 +0330

    last change

--- cypress/e2e/UI/kidzy/TestCase3.cy.js
+++ cypress/e2e/UI/kidzy/TestCase3.cy.js
@@ -2,15 +2,15 @@
 
 describe("TestSuite3", () => {
   it("testCase1", () => {
-    cy.visit("https://kidzy.land"),
-      cy.contains("همکاری با کیدزی"),
-      
-      //cy.contains("a[class = 'rounded-full']", "همکاری با کیدزی"),
-      // cy.contains("a[class='rounded-full']", "همکاری با کیدزی").click();
-        /*
-      cy.get(".relative > .intro-first").contains("هوشمند").click();
-      cy.get(".bg-gradient-to-b > .rounded-full").contains("همکاری با کیدزی").click();
-      cy.get(".rounded-full").contains("همکاری با کیدزی").click();
-      */
+    cy.visit("https://kidzy.land"), cy.contains("همکاری با کیدزی");
+
+    //cy.contains("a[class = 'rounded-full']", "همکاری با کیدزی"),
+    // cy.contains("a[class='rounded-full']", "همکاری با کیدزی").click();
+
+    cy.get(".relative > .intro-first").contains("هوشمند").click();
+    cy.get(".bg-gradient-to-b > .rounded-full")
+      .contains("همکاری با کیدزی")
+      .click();
+    cy.get(".rounded-full").contains("همکاری با کیدزی").click();
   });
 });
