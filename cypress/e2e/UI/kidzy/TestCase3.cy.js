083206ecf111c6fc10995193e2d9425dcb713ce1
armanabbassii <arman.abbasi.nt@gmail.com>
2023-03-18 10:12:02 +0330

    change1

--- cypress/e2e/UI/kidzy/TestCase3.cy.js
+++ cypress/e2e/UI/kidzy/TestCase3.cy.js
@@ -5,8 +5,9 @@ describe("TestSuite3", () => {
     cy.visit("https://kidzy.land"),
       cy.contains("همکاری با کیدزی"),
       //cy.contains("a[class = 'rounded-full']", "همکاری با کیدزی"),
-      cy.contains("a[class='rounded-full']", "همکاری با کیدزی").click();
-    /* 
+      // cy.contains("a[class='rounded-full']", "همکاری با کیدزی").click();
+        /*
+      cy.get(".relative > .intro-first").contains("هوشمند").click();
       cy.get(".bg-gradient-to-b > .rounded-full").contains("همکاری با کیدزی").click();
       cy.get(".rounded-full").contains("همکاری با کیدزی").click();
       */
