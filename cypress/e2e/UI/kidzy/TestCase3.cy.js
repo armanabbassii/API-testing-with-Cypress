3d2b52091e3c04ad532c8b277c27de5f4f207258
armanabbassii <arman.abbasi.nt@gmail.com>
2023-03-19 23:48:25 +0330

    question

--- cypress/e2e/UI/kidzy/TestCase3.cy.js
+++ cypress/e2e/UI/kidzy/TestCase3.cy.js
@@ -4,8 +4,8 @@ describe("TestSuite3", () => {
   it("testCase1", () => {
     cy.visit("https://kidzy.land"), cy.contains("همکاری با کیدزی");
 
-    //cy.contains("a[class = 'rounded-full']", "همکاری با کیدزی"),
-    // cy.contains("a[class='rounded-full']", "همکاری با کیدزی").click();
+    //??cy.contains("a[class = 'rounded-full']", "همکاری با کیدزی"),
+    // ??cy.contains("a[class='rounded-full']", "همکاری با کیدزی").click();
 
     cy.get(".relative > .intro-first").contains("هوشمند").click();
     cy.get(".bg-gradient-to-b > .rounded-full")
