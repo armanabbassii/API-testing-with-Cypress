aba264d3ccf8aabd4476e5fd75676e524d946e28
armanabbassii <arman.abbasi.nt@gmail.com>
2023-04-17 10:31:45 +0430

    commit

--- cypress/e2e/UI/CMS/simpleTest.cy.js
+++ cypress/e2e/UI/CMS/simpleTest.cy.js
@@ -1,5 +1,6 @@
 /// <reference types = "cypress"/>
 
+/*
 it("testcase1", () => {
   cy.visit("https://www.google.com/");
   cy.get("#APjFqb").type("wikipedia{enter}");
@@ -7,5 +8,32 @@ it("testcase1", () => {
 
   cy.origin("https://www.wikipedia.org/", () => {
     cy.get("#searchInput").type("Test done");
+  });
+});
+*/
+/*
+it("testcase1", () => {
+  cy.visit("https://www.google.com/");
+  cy.get("#APjFqb").type("time{enter}");
+  cy.contains("time.ir").click();
+
+  cy.origin("https://www.time.ir/", () => {
+    cy.get(
+      ".ctl00_cphHeader_Sampa_Web_View_GeneralUI_HeaderLogoContol01cphHeader_3811_imgLogo"
+    );
+  });
+});
+*/
+Cypress.Commands.add("getLinkByText", (text) => {
+  cy.contains("a", text);
+});
+
+it("origin example", () => {
+  cy.visit("/");
+  cy.getLinkByText("Start a free trial");
+
+  cy.origin("https://www.google.com/", () => {
+    cy.visit("");
+    cy.getLinkByText("Sign in");
   });
 });
