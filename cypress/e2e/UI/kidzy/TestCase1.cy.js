cc4bc5124c510b856fbcc2c60c8fcf0956f9250c
armanabbassii <arman.abbasi.nt@gmail.com>
2023-06-10 23:35:05 +0330

    edit

--- cypress/e2e/UI/kidzy/TestCase1.cy.js
+++ cypress/e2e/UI/kidzy/TestCase1.cy.js
@@ -8,10 +8,8 @@ describe("Testsuite1", () => {
   it.only("TestCase2", () => {
     cy.visit("https://kidzy.land/aboutus");
     cy.get("input[placeholder = 'نام و نام خانوادگی']").type("armanabbasi");
-    cy.get("#phone").type("09194292570");
-    cy.get("#email")
-      .type("arman.abbasi.nr@gmail.com")
-      .should("class", "rounded-4xl");
+    cy.get("#phone").type("09194442255");
+    cy.get("#email").type("arman@gmail.com").should("class", "rounded-4xl");
     cy.get(".resize-none").type("Hi. this is Kidzy.land");
     cy.get('input[placeholder="متن داخل تصویر"]').type("1122233");
     cy.get('input[value="ارسال پیام"]').click();
