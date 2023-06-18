541cd7178c6056f5b69385feb336de044a47d3e6
armanabbassii <arman.abbasi.nt@gmail.com>
2023-06-18 14:38:22 +0330

    api

---
+++ cypress/e2e/API/APITesting(SDET)/httpRequest.cy.js
@@ -0,0 +1,8 @@
+/// <reference types = "cypress"/>
+
+describe("https requests", () => {
+  it("get call", () => {
+    //for send any type of request, write cy.request model        (type of request , "url beteen double quatation")
+    //we need to verify something use .its
+  });
+});
