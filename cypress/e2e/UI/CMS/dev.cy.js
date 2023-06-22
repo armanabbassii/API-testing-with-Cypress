26ad340a02b4e217fc6c0a54d7f6b9c12e133f1f
armanabbassii <arman.abbasi.nt@gmail.com>
2023-06-22 20:08:10 +0330

    cy.window

--- cypress/e2e/UI/CMS/dev.cy.js
+++ cypress/e2e/UI/CMS/dev.cy.js
@@ -1,13 +1,41 @@
-/// <reference types = "cypress"/>
+/*/// <reference types = "cypress"/>
 
-import { PASSWORD, Password, SSOLOGIN, USERNAME } from "./constans";
+import { PASSWORD, USERNAME } from "./constans";
 
 describe("TestSuite1", () => {
   it("testcase1", () => {
     cy.visit("http://dev.cms.test/");
     cy.get(".login-btn").contains("ورود با نام کاربری").click();
+    cy.wait(15000);
 
-    cy.origin(SSOLOGIN, () => {
+    cy.origin("https://accounts.pod.ir", () => {
+      cy.visit(
+        "/oauth2/authorize/index.html?client_id=17959574q2f0347718971594ccd86f3f4&response_type=code&redirect_uri=http://dev.cms.test/login?redirect=true&code_challenge_method=S256&code_challenge=cgQZyBBAg6XSiBZZ1VBETnhKHjnIu18jOYOPqsG6Edw&scope=profile&prompt=login"
+      );
+      cy.get("input[name=identity]").type(USERNAME);
+      cy.get("#authPassword-inp").type(PASSWORD);
+      cy.get("#authLoginBtn").click();
+    });
+  });
+});
+*/
+
+/// <reference types="cypress"/>
+
+import { PASSWORD, USERNAME } from "./constans";
+
+describe("TestSuite1", () => {
+  it("testcase1", () => {
+    cy.visit("http://dev.cms.test/");
+    cy.get(".login-btn").contains("ورود با نام کاربری").click();
+    cy.wait(15000);
+
+    // Switch to the new window
+    cy.window().then((win) => {
+      const newWindowUrl = win.location.href;
+      cy.visit(newWindowUrl);
+
+      // Perform actions on the new window
       cy.get("input[name=identity]").type(USERNAME);
       cy.get("#authPassword-inp").type(PASSWORD);
       cy.get("#authLoginBtn").click();
@@ -16,8 +44,6 @@ describe("TestSuite1", () => {
 });
 
 /*
-
-
   it("2-test2", function () {
     cy.origin("https://accounts.pod.ir", () => {
       cy.get("input[name=identity]").type("Armanabbasi");
