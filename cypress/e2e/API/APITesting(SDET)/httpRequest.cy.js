effca472726b55069113709150d97f4a7e58d32e
armanabbassii <arman.abbasi.nt@gmail.com>
2023-07-11 09:58:46 +0330

    api

--- cypress/e2e/API/APITesting(SDET)/httpRequest.cy.js
+++ cypress/e2e/API/APITesting(SDET)/httpRequest.cy.js
@@ -2,10 +2,11 @@
 
 describe("Session17_ApiAutomation", () => {
   it("SimpleRequests", () => {
-    cy.request("https://restcountries.com/v3.1/name/germany").then(
+    cy.request("https://restcountries.com/v3.1/name/australia").then(
       ($response) => {
+        //  میتونیم دریافتش کنیم .thenما الان اینجا ابجکت ریسپانس رو داریم،از طریق
         expect($response.status).to.eq(200);
-        expect($response.body[0].capital[0]).eq("Berlin");
+        expect($response.body[0].capital[length]).eq("Berlin");
       }
     );
   });
