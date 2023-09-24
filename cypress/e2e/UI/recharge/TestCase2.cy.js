19027553b8938800eac121ca0e63b38bcf929817
armanabbassii <arman.abbasi.nt@gmail.com>
2023-09-24 10:31:11 +0330

    deleted junk files

--- cypress/e2e/UI/recharge/TestCase2.cy.js
+++
@@ -1,9 +0,0 @@
-/// <reference types = 'cypress'/>
-
-describe("TestSuits2", () => {
-  it("Testcase1", () => {
-    cy.visit("https://company.recharge.com/");
-    cy.get(".menu-toggle").click();
-    //cy.get(".menu-item" > ".menu-label").click(); //should("contain", "company");
-  });
-});
--- cypress/e2e/UI/wikiperdia/session4(sync$async).js
+++
@@ -1,8 +0,0 @@
-/// <reference types ="cypress"/>
-
-describe("first unit test", () => {
-  it("testcase1", () => {
-    cy.visit("https://www.google.com/");
-    cy.ger;
-  });
-});
